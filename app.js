d453690812653de7154829633672240672254622
Oscar Saltijeral Venegas <oscsalven@gmail.com>
2022-01-05 15:58:08 -0600

    Fixed playRound using a switch statement

--- app.js
+++ app.js
@@ -13,21 +13,42 @@ function computerPlay() {
 /* The computer selection will be stored here */
 let computerSelection = computerPlay();
 
-/* This function will ask the player to pick an option */
-
 /* This will store the player's selection */
-let playerSelection = "rock";
+let playerSelection = prompt("Please choose your weapon: ").toLowerCase();
 
 /* Play a single round */
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "paper") {
-        console.log("You lose.");
-    } else if (computerSelection === "scissors") {
-        console.log("You win.");
-    } else if (computerSelection == playerSelection) {
-        console.log("Tie");
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "paper") {
+                console.log("You Lose. Paper beats rock.");
+            } else if (computerSelection === "scissors") {
+                console.log("You Win!");
+            }
+            break;
+
+        case "paper":
+            if (computerSelection === "scissors") {
+                console.log("You Lose. Scissors beat paper.");
+            } else if (computerSelection === "rock") {
+                console.log("You Win!");
+            }
+            break;
+
+        case "scissors":
+            if (computerSelection === "rock") {
+                console.log("You Lose. Rock beats scissors");
+            } else if (computerSelection === "paper") {
+                console.log("You Win!");
+            }
+            break;
+
+        default:
+            console.log("It's a tie.");
+            break;
     }
 }
 
+console.log(playerSelection);
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection)); /* Test for playRound */
